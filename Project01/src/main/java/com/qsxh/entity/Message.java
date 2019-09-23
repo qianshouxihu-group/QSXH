1082bc45c01915bd8a45292baa4c61077f16427e
chongjin14 <jingchao14@126.com>
2019-09-23 09:08:49 +0800

    test0923 0908

---
+++ Project01/src/main/java/com/qsxh/entity/Message.java
@@ -0,0 +1,64 @@
+package com.qsxh.entity;
+
+import java.sql.Timestamp;
+
+public class Message {
+
+    private String messageId;
+    private String fromId;
+    private String fromName;
+    private String toId;
+    private String messageText;
+    private Timestamp messageDate;
+
+    public Message() {
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    public String getFromName() {
+        return fromName;
+    }
+
+    public void setFromName(String fromName) {
+        this.fromName = fromName;
+    }
+
+    public String getToId() {
+        return toId;
+    }
+
+    public void setToId(String toId) {
+        this.toId = toId;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public Timestamp getMessageDate() {
+        return messageDate;
+    }
+
+    public void setMessageDate(Timestamp messageDate) {
+        this.messageDate = messageDate;
+    }
+}
