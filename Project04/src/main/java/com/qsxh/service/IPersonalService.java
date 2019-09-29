05e56fd800b8cc845fc733b24f0d7d0f2ce06519
chongjin14 <jingchao14@126.com>
2019-09-29 16:10:21 +0800

    20190929 16:10

--- Project04/src/main/java/com/qsxh/service/IPersonalService.java
+++
@@ -1,9 +0,0 @@
-package com.qsxh.service;
-
-import com.qsxh.entity.PersonalData;
-
-public interface IPersonalService {
-
-    //完善注册信息
-    public Integer improvingRegData(PersonalData personalData);
-}
